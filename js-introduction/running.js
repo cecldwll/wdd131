1f026c8f93c722e86791cc275622524b26265206
cecldwll <thaumaturgy101@gmail.com>
2024-04-23 10:34:51 -0600

    js introduction activities

---
+++ js-introduction/running.js
@@ -0,0 +1,4 @@
+const apples = 5;
+const oranges = 3;
+let total = apples + oranges;
+console.log("total:", total); //console.log is a builtin Javascript browser tool for outputting (logging) variables to a console
