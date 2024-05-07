5f14c4b9e59c425097dc7201f17106252bae82a6
cecldwll <thaumaturgy101@gmail.com>
2024-05-06 23:50:43 -0600

    practice assignments (debugging and more dom practice stuff)

---
+++ js-debugging/scripts/debugging.js
@@ -0,0 +1,22 @@
+// Activity 1
+// const pi = 3.14;
+// let radius = 3;
+// let area = 0;
+// area = radius * radius * pi;
+// console.log(area);
+// radius = 4;
+// area = radius * radius * pi;
+// console.log(area)
+
+const pi = 3.14;
+let area = 0;
+
+function circleArea(radius) {
+  const area = radius * radius * pi;
+  return area;
+}
+
+area = circleArea(3);
+console.log("Area1:", area);
+area = circleArea(4);
+console.log("Area2:", area);
